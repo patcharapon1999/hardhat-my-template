40ce57ea2cd409ab2077ac576679d4d5ab31f0d0
Poonsak Aphidetmongkhon <poonsak_lds@hotmail.com>
2022-07-25 23:17:57 +0700

    setup eslint and prettier.

--- deploy/001_deploy_token.ts
+++ deploy/001_deploy_token.ts
@@ -1,17 +1,17 @@
-import {HardhatRuntimeEnvironment} from 'hardhat/types';
-import {DeployFunction} from 'hardhat-deploy/types';
+import { HardhatRuntimeEnvironment } from "hardhat/types"
+import { DeployFunction } from "hardhat-deploy/types"
 
 const func: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
-  const {deployments, getNamedAccounts} = hre;
-  const {deploy} = deployments;
+  const { deployments, getNamedAccounts } = hre
+  const { deploy } = deployments
 
-  const {deployer, tokenOwner} = await getNamedAccounts();
+  const { deployer, tokenOwner } = await getNamedAccounts()
 
-  await deploy('Token', {
+  await deploy("Token", {
     from: deployer,
     args: [tokenOwner],
     log: true,
-  });
-};
-export default func;
-func.tags = ['Token'];
+  })
+}
+export default func
+func.tags = ["Token"]
